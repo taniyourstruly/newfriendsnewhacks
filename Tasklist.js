5a60a1221f1d1b2f92ce7a863407fd840a1c2e35
tommycheung24 <63329022+tommycheung24@users.noreply.github.com>
2020-09-26 17:49:48 -0400

    update tasklist.js

--- Tasklist.js
+++ Tasklist.js
@@ -1,5 +1,6 @@
-function Task(title, hours, minutes, month, day, year){
+function Task(title, info, hours, minutes, month, day, year){
     this.title = title;
+    this.info = info;
     this.hours = hours;
     this.minutes = minutes;
     this.month = month;
@@ -13,7 +14,27 @@ function accessTask(index){
     return tasklist[index];
 }
 
-function createTask(title, hours, minutes, date){
-    sDate = date.split("/");
-    taskList.push(new task(title, hours, minutes, Number(sDate[0]), Number(sDate[1]), Number(sDate[2]))); 
+function createTask(){
+    let title = document.getElementById("title").value;
+    let info = document.getElementById("notes").value;
+    let date = document.getElementById("party").value;
+    //date format : "2020-09-01T12:13"
+    let year = Number(date.substring(0,4));
+    let month =Number(date.substring(5,7));
+    let day = Number(date.substring(8,10));
+    let hours = Number(date.substring(11, 13));
+    let minutes = Number(date.substring(14));
+
+    taskList.push(new Task(title, info, hours, minutes, month, day, year)); 
 }
+
+function actionTime(task){
+    let current = new Date();
+    if(current.getFullYear() ===  task.year && current.getMonth() === task.month && current.getDate === task.day){
+        let diff = (current.getHours()*60 + current.getMinutes()) - (task.hours*60 + task.minutes);
+        if(diff === 30 ){
+            return true;
+        }
+    }
+}
+
